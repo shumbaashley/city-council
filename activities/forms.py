f45d03608e1fd60d52539f02a7a1ff4028b6d97a
shumbaashley <ashleytshumba@gmail.com>
2021-05-13 16:57:25 +0200

    removed nonsense

--- activities/forms.py
+++ activities/forms.py
@@ -4,31 +4,6 @@ from .models import WeeklyPerfomanceReview, PerfomanceReview
 from django.utils.translation import ugettext_lazy as _
 
 class WeeklyPerfomanceReviewForm(ModelForm):
-    # activity2 = forms.CharField(required=False)
-    # activity3 = forms.CharField(required=False)
-    # activity4 = forms.CharField(required=False)
-    # activity5 = forms.CharField(required=False)
-    # activity6 = forms.CharField(required=False)
-    # activity7 = forms.CharField(required=False)
-
-    # work_to_be_done2 = forms.CharField(required=False)
-    # work_to_be_done3 = forms.CharField(required=False)
-    # work_to_be_done4 = forms.CharField(required=False)
-    # work_to_be_done5 = forms.CharField(required=False)
-    # work_to_be_done6 = forms.CharField(required=False)
-    # work_to_be_done7 = forms.CharField(required=False)
-
-    # work_done1  = forms.CharField(required=False)
-    # work_done2  = forms.CharField(required=False)
-    # work_done3  = forms.CharField(required=False)
-    # work_done4  = forms.CharField(required=False)
-    # work_done5  = forms.CharField(required=False)
-
-    # evidence_of_work_done1 = forms.CharField(required=False)
-    # evidence_of_work_done2 = forms.CharField(required=False)
-    # evidence_of_work_done3 = forms.CharField(required=False)
-    # evidence_of_work_done4 = forms.CharField(required=False)
-    # evidence_of_work_done5 = forms.CharField(required=False)
     class Meta:
         model = WeeklyPerfomanceReview
         fields = '__all__'
@@ -65,8 +40,8 @@ class WeeklyPerfomanceReviewForm(ModelForm):
             'innovations': Textarea(attrs={'cols': 20, 'rows': 5}),
         }
         help_texts = {
-            'week_starting': _('Use format DD/MM/YYYY.'),
-            'week_ending': _('Use format DD/MM/YYYY.'),
+            'week_starting': _('Use format YYYY-MM-DD.'),
+            'week_ending': _('Use format YYYY-MM-DD.'),
         }
 
 class PerfomanceReviewForm(ModelForm):
